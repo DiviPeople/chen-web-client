5247a2aaee8c838679ec27ffe66a053aca13855b
Nsenixx <sluppystr@gmail.com>
2023-06-02 14:26:11 +0300

    Add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+  import {Card, Button, Label, Input} from 'flowbite-svelte'
+
+  let isShown = false
+  function toggle() { isShown = !isShown }
+</script>
+
+<Card class="mt-40 m-auto dark:bg-neutral-900 dark:border-zinc-800">
+  <form class="flex flex-col space-y-6 dark:bg-neutral-900" action="/">
+    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in</h3>
+    <Label class="space-y-2">
+      <span>Email</span>
+      <Input type="email" name="email" placeholder="example@email.com" required />
+    </Label>
+    <Label class="space-y-2">
+      <span>Your password</span>
+      <Input type="password" name="password" placeholder="•••••••••" required />
+    </Label>
+    <Button href="/admin" type="submit" class="w-full dark:bg-yellow-500 dark:hover:bg-amber-500">Login to your account</Button >
+    <Button type="submit" class="w-full dark:bg-gray-600 dark:hover:bg-gray-500" on:click={toggle}>Forgot password?</Button>
+  </form>
+</Card>
+
+{#if isShown}
+  <div>
+    <Card class="mt-2 m-auto dark:bg-neutral-900 dark:border-zinc-800">
+      <form class="flex flex-col space-y-6 bg-neutral-900" action="/">
+        <h3 class="text-xl font-medium text-gray-900 dark:text-white">Enter your email</h3>
+        <Label class="space-y-2">
+          <Input type="email" name="email" placeholder="name@company.com" required />
+        </Label>
+        <Button type="submit" class="w-full dark:bg-gray-600 dark:hover:bg-gray-500">Send password</Button>
+      </form>
+    </Card>
+  </div>
+{/if}

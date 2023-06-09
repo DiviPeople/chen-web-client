9e8062b7a5df2fc2b035ce1c27e0f66ac792dded
German Drozdenko <german.drozdenko@gmail.com>
2023-06-09 17:18:34 +0300

    Implement environment variables

--- src/routes/admin/+page.ts
+++ src/routes/admin/+page.ts
@@ -1,33 +1,34 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import {redirect} from '@sveltejs/kit'
+import type {PageLoad} from './$types'
+import {env} from '$env/dynamic/public'
 
-export const load = (async ({ fetch }) => {
-    const response = await fetch('http://localhost:8080/me', {
-        credentials: 'include'
+export const load = (async ({fetch}) => {
+  const response = await fetch(`${env.PUBLIC_API_URL}/me`, {
+    credentials: 'include',
+  })
+  const result = await response.json()
+  if (!result.is_staff) {
+    throw redirect(308, `${env.PUBLIC_CLIENT_URL}`)
+  }
+
+  const me = async () => {
+    const response = await fetch(`${env.PUBLIC_API_URL}/me`, {
+      credentials: 'include',
     })
     const result = await response.json()
-    if (!result.is_staff) {
-        throw redirect(308, 'http://localhost:5173/')
-    }
+    return result
+  }
 
-    const me = async () => {
-        const response = await fetch('http://localhost:8080/me', {
-            credentials: 'include'
-        })
-        const result = await response.json()
-        return result
-    }
+  const users = async () => {
+    const response = await fetch(`${env.PUBLIC_API_URL}/users`, {
+      credentials: 'include',
+    })
+    const result = await response.json()
+    return result
+  }
 
-    const users = async () => {
-        const response = await fetch('http://localhost:8080/users', {
-            credentials: 'include'
-        })
-        const result = await response.json()
-        return result
-    }
-
-    return {
-        me: me(),
-        users: users()
-    }
-}) satisfies PageLoad;
+  return {
+    me: me(),
+    users: users(),
+  }
+}) satisfies PageLoad

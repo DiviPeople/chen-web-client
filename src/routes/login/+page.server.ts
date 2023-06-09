72dc48c3bee1716d0080f07660ebad13d6a3803c
German Drozdenko <german.drozdenko@gmail.com>
2023-06-09 14:27:31 +0300

    Implement login request to API

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,44 @@
+import { env } from '$env/dynamic/public'
+import type { Actions } from './$types'
+import { redirect } from '@sveltejs/kit'
+import jwtDecode from 'jwt-decode'
+
+export interface JwtToken {
+  sub: number,
+  is_staff: boolean,
+  is_superuser: boolean,
+  iat: number,
+  exp: number,
+}
+
+export const actions = {
+  default: async ({ request, cookies }) => {
+    const formData = await request.formData()
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    const response = await fetch(`${env.PUBLIC_API_URL}/login`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      })
+    })
+
+    const result = await response.json()
+
+    if (response.ok) {
+      cookies.set('token', result, {maxAge: 3600})
+      const decoded: JwtToken = jwtDecode(result)
+      if (decoded.is_staff) {
+        throw redirect(303, `${env.PUBLIC_CLIENT_URL}/admin`)
+      } else {
+        throw redirect(303, `${env.PUBLIC_CLIENT_URL}`)
+      }
+    }
+  }
+} satisfies Actions;

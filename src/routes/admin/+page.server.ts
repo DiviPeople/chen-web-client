c34728b832ab95c5d777dbb38d39edefebbb016a
German Drozdenko <german.drozdenko@gmail.com>
2023-06-14 16:38:03 +0300

    Implement user deletion request to API

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -24,4 +24,23 @@ export const actions = {
       })
     }
   },
+
+  delete_user: async ({request, cookies}: RequestEvent) => {
+    const formData = await request.formData()
+    const token = cookies.get('token')
+
+    if (typeof token === 'string') {
+      await fetch(`${env.PUBLIC_API_URL}/user`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `token=${token}`,
+        },
+        body: JSON.stringify({
+          user_name: formData.get('delete_login'),
+        }),
+      })
+    }
+  },
 } satisfies Actions

ef1dbb082fa464915239906336a0e3f716d12f01
German Drozdenko <german.drozdenko@gmail.com>
2023-06-19 16:32:12 +0300

    Refactor admin page load functions

--- src/routes/admin/+page.ts
+++ src/routes/admin/+page.ts
@@ -1,28 +1,7 @@
-import {redirect} from '@sveltejs/kit'
 import type {PageLoad} from './$types'
 import {env} from '$env/dynamic/public'
 
 export const load = (async ({fetch}) => {
-  const response = await fetch(`${env.PUBLIC_API_URL}/me`, {
-    credentials: 'include',
-  })
-  if (!response.ok) {
-    throw redirect(308, `${env.PUBLIC_CLIENT_URL}`)
-  }
-
-  const result = await response.json()
-  if (!result.is_staff) {
-    throw redirect(308, `${env.PUBLIC_CLIENT_URL}`)
-  }
-
-  const me = async () => {
-    const response = await fetch(`${env.PUBLIC_API_URL}/me`, {
-      credentials: 'include',
-    })
-    const result = await response.json()
-    return result
-  }
-
   const users = async () => {
     const response = await fetch(`${env.PUBLIC_API_URL}/users`, {
       credentials: 'include',
@@ -32,7 +11,6 @@ export const load = (async ({fetch}) => {
   }
 
   return {
-    me: me(),
     users: users(),
   }
 }) satisfies PageLoad
